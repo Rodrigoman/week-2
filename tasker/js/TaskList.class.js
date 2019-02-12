ee4b09253793ae36e1c8e1f8186937d917c31577
= <=>
2019-02-12 14:38:10 -0600

    remove a taskt

--- tasker/js/TaskList.class.js
+++ tasker/js/TaskList.class.js
@@ -4,4 +4,13 @@ export default class TaskList {
   constructor() {
     this.taskList = Storage.getParsedTaskList();
   }
+
+  /**
+   * removes a task from TaskList
+   * @param taskId the id to remove
+   */
+  removeFromTaskList(taskId) {
+    this.taskList = this.taskList.filter(task => task.id !== taskId);
+    Storage.saveTaskList(this.taskList);
+  }
 }
