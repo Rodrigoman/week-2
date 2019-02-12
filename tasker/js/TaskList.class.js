3f11cc1c4d760b7908da71c9b1867a84d019a308
= <=>
2019-02-12 17:53:02 -0600

    update and sort filters based on filter class

--- tasker/js/TaskList.class.js
+++ tasker/js/TaskList.class.js
@@ -1,8 +1,11 @@
 import Storage from './Storage.class.js';
+import Filters from './filters.class.js';
 
 export default class TaskList {
   constructor() {
     this.taskList = Storage.getParsedTaskList();
+    this.currentFilters = new Filters();
+    this.filteredTaskList = this.taskList;
   }
 
   /**
@@ -12,5 +15,21 @@ export default class TaskList {
   removeFromTaskList(taskId) {
     this.taskList = this.taskList.filter(task => task.id !== taskId);
     Storage.saveTaskList(this.taskList);
+  }
+
+  sortTasktList() {
+    const filter = this.currentFilters;
+    this.filteredTaskList = this.taskList.filter((task) => {
+      if (filter.status !== 'All') {
+        return (task.status === filter.status
+           && task.name.toLowerCase().includes(filter.search.toLowerCase()));
+      }
+      return task.name.toLowerCase().includes(filter.search.toLowerCase());
+    });
+  }
+
+  updateSortStatus(status) {
+    this.currentFilters.status = status;
+    this.sortTasktList();
   }
 }
