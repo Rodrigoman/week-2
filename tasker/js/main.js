51d26230edbc70753835f481eae2494488a97dfc
= <=>
2019-02-13 10:11:06 -0600

    main file

--- tasker/js/main.js
+++ tasker/js/main.js
@@ -6,15 +6,34 @@ class Jarvis {
   }
 
   /**
-   * renders the UI
+   * renders the UI if you ask nicely
    */
   giveMeTheUI(magicWord) {
     if (magicWord === 'please') {
       this.UI.renderTaskTable();
+      return this;
     }
+    return this;
+  }
+
+  makeTheStatusFilterWork() {
+    this.UI.addListenerToStatusRadios();
+    return this;
+  }
+
+  TheSortFilter() {
+    this.UI.addListenerToSortRadios();
+    return this;
+  }
+
+  andTheSearch() {
+    this.UI.addListenerToSearchForm();
   }
 }
 
 const jarvis = new Jarvis('#pokedex tbody');
 
-jarvis.giveMeTheUI('please');
+jarvis.giveMeTheUI('please')
+  .makeTheStatusFilterWork()
+  .TheSortFilter()
+  .andTheSearch();
