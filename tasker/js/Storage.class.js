bfdbe72bf4872b56fb90653bc109015da63f61e8
= <=>
2019-02-14 12:00:31 -0600

    demo task

--- tasker/js/Storage.class.js
+++ tasker/js/Storage.class.js
@@ -2,13 +2,25 @@ class Storage {
   constructor(from) {
     this.db = localStorage;
     this.from = from;
+    this.dummy = [{
+      id: 1,
+      name: 'this is a demo task go ahead',
+      assignee: 'and create a new one',
+      status: 'Done',
+      date: new Date('1999/9/9'),
+    }];
   }
 
   /**
    * gets and parse the TaskList from Local Storage
    */
   getParsedTaskList() {
-    return JSON.parse(this.db.getItem(this.from));
+    const result = JSON.parse(this.db.getItem(this.from));
+
+    if (result === null || result.length === 0) {
+      return this.dummy;
+    }
+    return result;
   }
 
   /**
