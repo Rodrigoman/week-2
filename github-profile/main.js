c9d4dbc3ce57ade61a0d325f57a175b18b26a31e
= <=>
2019-02-14 11:30:57 -0600

    github class

--- github-profile/main.js
+++ github-profile/main.js
@@ -1,1 +1,27 @@
-console.log('hey');
+class GitHubFy {
+  constructor() {
+    this.headers = {
+      Authorization: 'token 7780b41a1bbd1a474133226c842cebaaba84e84d',
+    };
+    this.searchForm = document.querySelector('#searchForm');
+  }
+
+  search(query) {
+    fetch(`https://api.github.com/users/${query}`, { headers: this.headers })
+      .then((data) => {
+        this.user = data.json();
+      });
+  }
+
+  listenToSearchAction() {
+    this.searchForm.addEventListener('submit', (event) => {
+      event.preventDefault();
+      const query = document.querySelector('#user').value;
+      this.search(query);
+    });
+  }
+}
+
+
+const github = new GitHubFy();
+github.listenToSearchAction();
